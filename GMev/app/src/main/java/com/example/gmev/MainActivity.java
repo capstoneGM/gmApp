05fdb68a52ee3dc5ad65574f8b4a98fe130a8394
scanales01 <scanal01@outlook.com>
2022-01-29 15:46:28 -0600

    Update to MainActivity(Sign in)
    Added Firebase Authentication to Sign in.

--- GMev/app/src/main/java/com/example/gmev/MainActivity.java
+++ GMev/app/src/main/java/com/example/gmev/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gmev;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,13 +9,22 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
 
         setContentView(R.layout.activity_main);
 
@@ -30,14 +40,19 @@ public class MainActivity extends AppCompatActivity {
 
         loginbtn.setOnClickListener((new View.OnClickListener() {
             @Override
+//            public void onClick(View view) {
+//                if(username.getText().toString().equals("team1") && password.getText().toString().equals("team1")){
+//                    //Toast.makeText(MainActivity.this, "LOGIN SUCCESSFULL", Toast.LENGTH_SHORT).show();
+//                    openMainMenu();
+//                }else {
+//                    Toast.makeText(MainActivity.this, "your user name or password are not correct", Toast.LENGTH_SHORT).show();
+//
+//                }
+//            }
             public void onClick(View view) {
-                if(username.getText().toString().equals("team1") && password.getText().toString().equals("team1")){
-                    //Toast.makeText(MainActivity.this, "LOGIN SUCCESSFULL", Toast.LENGTH_SHORT).show();
-                    openMainMenu();
-                }else {
-                    Toast.makeText(MainActivity.this, "your user name or password are not correct", Toast.LENGTH_SHORT).show();
-
-                }
+                String email = username.getText().toString();
+                String pass = password.getText().toString();
+                signIn(email, pass);
             }
         }));
         singupbtn.setOnClickListener((new View.OnClickListener() {
@@ -48,6 +63,19 @@ public class MainActivity extends AppCompatActivity {
         }));
 
 
+    }
+
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            openMainMenu();
+                        }
+                    }
+                });
     }
     public void openRegistration(){
         Intent intent = new Intent(this, Registration.class);
