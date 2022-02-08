bb957994ee254b887d21432d49dee69aeb29c3b7
scanales01 <scanal01@outlook.com>
2022-02-08 15:48:04 -0600

    Updated Main Menu Functionality
    Allows users to connect to car.

--- GMev/app/src/main/java/com/example/gmev/MainMenu.java
+++ GMev/app/src/main/java/com/example/gmev/MainMenu.java
@@ -5,25 +5,128 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.card.MaterialCardView;
 
-import java.net.URI;
+import com.smartcar.sdk.SmartcarAuth;
+import com.smartcar.sdk.SmartcarCallback;
+import com.smartcar.sdk.SmartcarResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainMenu extends AppCompatActivity {
+    private static String CLIENT_ID;
+    private static String REDIRECT_URI;
+    private static String[] SCOPE;
+    private static SmartcarAuth smartcarAuth;
+    private int charge = 0;
+    private float carRange = 0;
+    private String carMake;
+    private String carModel;
+    private int carYear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
+        //Charge bar objects
+        final ProgressBar chargeBar = findViewById(R.id.charge_bar);
+        final TextView chargePercent = findViewById(R.id.charge_num);
+        final TextView make = findViewById(R.id.vehicle_make);
+        final TextView modelYear = findViewById(R.id.vehicle_model_year);
+        final TextView range = findViewById(R.id.vehicle_range);
+
+        //Card buttons
         MaterialCardView profile = (MaterialCardView) findViewById(R.id.profile_card);
         MaterialCardView location = (MaterialCardView) findViewById(R.id.charger_card);
         MaterialCardView bank = (MaterialCardView) findViewById(R.id.bank_card);
         MaterialCardView logOut = (MaterialCardView) findViewById(R.id.logout_card);
+        MaterialCardView connection = (MaterialCardView) findViewById(R.id.connect_card);
+        MaterialCardView carInfo = (MaterialCardView) findViewById(R.id.vehicle_card);
 
+        //SmartCar initialization
+        CLIENT_ID = getString(R.string.client_id);
+        REDIRECT_URI = getString(R.string.smartcar_auth_scheme) + "://" + getString(R.string.smartcar_auth_host);
+        SCOPE = new String[]{"required:read_vehicle_info", "required:read_battery"};
+
+        smartcarAuth = new SmartcarAuth(
+                CLIENT_ID,
+                REDIRECT_URI,
+                SCOPE,
+                true,
+                new SmartcarCallback() {
+                    @Override
+                    public void handleResponse(final SmartcarResponse smartcarResponse) {
+                        Log.d("SmartcarAuth", "authorization code: " + smartcarResponse.getCode());
+
+                        final OkHttpClient client = new OkHttpClient();
+
+                        //Create new thread for smartCar requests
+                        new Thread(() -> {
+                            //Send request to exchange auth code for access token
+                            Request exchangeRequest = new Request.Builder()
+                                    .url(getString(R.string.app_server) + "/exchange?code=" + smartcarResponse.getCode())
+                                    .build();
+
+                            try {
+                                client.newCall(exchangeRequest).execute();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+
+                            //Send request to retrieve info
+                            Request infoRequest = new Request.Builder()
+                                    .url(getString(R.string.app_server) + "/vehicle")
+                                    .build();
+
+                            try {
+                                Response response = client.newCall(infoRequest).execute();
+                                String jsonBody = response.body().string();
+                                JSONObject JObject = new JSONObject(jsonBody);
+
+                                Float chargeDec = Float.valueOf(JObject.getString("percentRemaining")).floatValue() * 100;
+                                charge = Math.round(chargeDec);
+                                carRange = Float.valueOf(JObject.getString("range")).floatValue();
+                                carMake = JObject.getString("make");
+                                carModel = JObject.getString("model");
+                                carYear = JObject.getInt("year");
+
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    connection.setVisibility(View.GONE);
+                                    updateCharge(chargeBar, chargePercent, range);
+                                    updateVehicle(make, modelYear);
+                                    carInfo.setVisibility(View.VISIBLE);
+                                }
+                            });
+                        }).start();
+                    }
+                });
+
+        //Connect to car Button
+        smartcarAuth.addClickHandler(getApplicationContext(), connection);
+
+        //Profile Button
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,6 +134,7 @@ public class MainMenu extends AppCompatActivity {
             }
         });
 
+        //Locate Charger Button
         location.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,6 +142,7 @@ public class MainMenu extends AppCompatActivity {
             }
         });
 
+        //Manage Bank Button
         bank.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,37 +150,13 @@ public class MainMenu extends AppCompatActivity {
             }
         });
 
+        //Log Out Button
         logOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 logout();
             }
         });
-
-
-//        MaterialButton logoutbtn = (MaterialButton) findViewById(R.id.logoutbtn);
-//        MaterialButton charingstationsbtn = (MaterialButton) findViewById(R.id.charingstationsbtn);
-//        MaterialButton balancebtn = (MaterialButton) findViewById(R.id.balancebtn);
-//
-//        logoutbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                openMainActivity();
-//            }
-//        });
-//        charingstationsbtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//               // openGmMap();
-//                openGmMap("https://www.google.com/maps/search/electric+vehicles+charging+stations/@36.2031967,-94.1819611,12z/data=!3m1!4b1");
-//            }
-//        });
-//        balancebtn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                openAccountBalance();
-//            }
-//        });
     }
 
     private void openAccountBalance() {
@@ -95,6 +176,14 @@ public class MainMenu extends AppCompatActivity {
         finish();
     }
 
+    private void updateCharge(ProgressBar chargeBar, TextView chargePercent, TextView range) {
+        chargeBar.setProgress(charge);
+        chargePercent.setText(String.valueOf(charge) + "%");
+        range.setText(String.valueOf(carRange));
+    }
 
-
+    private void updateVehicle(TextView make, TextView modelYear) {
+        make.setText(carMake);
+        modelYear.setText(carModel + " " + carYear);
+    }
 }
