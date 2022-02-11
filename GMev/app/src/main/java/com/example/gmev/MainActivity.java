f4f90eb38d42fc97f8b12abfe2a1cce0829def3d
Khaled-Pr <54240938+Khaled-Pr@users.noreply.github.com>
2022-02-11 17:57:48 -0600

    Kd_SignIn and singUp Functionality

--- GMev/app/src/main/java/com/example/gmev/MainActivity.java
+++ GMev/app/src/main/java/com/example/gmev/MainActivity.java
@@ -1,17 +1,29 @@
 package com.example.gmev;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    EditText mEmail, mPassword;
+    MaterialButton loginbtn;
+    MaterialButton singupbtn;
+    ProgressBar progressbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,25 +31,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        TextView username = findViewById(R.id.username);
-        TextView password = findViewById(R.id.password);
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-        MaterialButton singupbtn = (MaterialButton) findViewById(R.id.signupbtn);
-
-
-        //Default username (team1) and password (team1)
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+        singupbtn = (MaterialButton) findViewById(R.id.signupbtn);
+        progressbar = findViewById(R.id.progressbar);
+        mAuth = FirebaseAuth.getInstance();
 
 
         loginbtn.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(username.getText().toString().equals("team1") && password.getText().toString().equals("team1")){
-                    //Toast.makeText(MainActivity.this, "LOGIN SUCCESSFULL", Toast.LENGTH_SHORT).show();
-                    openMainMenu();
-                }else {
-                    Toast.makeText(MainActivity.this, "your user name or password are not correct", Toast.LENGTH_SHORT).show();
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
 
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email address can not be blank");
+                    return;
                 }
+                if(TextUtils.isEmpty(password)){
+                    mPassword.setError("Password can not be blank");
+                    return;
+                }
+                if(password.length()<6){
+                    mPassword.setError("Password must be at least six characters");
+                    return;
+                }
+                progressbar.setVisibility(view.VISIBLE);
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Logged in.",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainMenu.class));
+                        }else{
+                            Toast.makeText(MainActivity.this, "your email or password are incorrect"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
             }
         }));
         singupbtn.setOnClickListener((new View.OnClickListener() {
@@ -53,9 +86,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Registration.class);
         startActivity(intent);
     }
-    public void openMainMenu(){
-        Intent intent2 = new Intent(this, MainMenu.class);
-        startActivity(intent2);
-    }
+
 
 }
--- GMev/app/src/main/java/com/example/gmev/Registration.java
+++ GMev/app/src/main/java/com/example/gmev/Registration.java
@@ -1,33 +1,91 @@
 package com.example.gmev;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Registration extends AppCompatActivity {
-
+    private FirebaseAuth mAuth;
+    EditText mFullName, mEmail, mPhoneNumber, mAddress,mVehicleModel, mYear, mVIN, mPassword1, mPassword2;
+    MaterialButton mSubmit;
+    ProgressBar mProgressbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        MaterialButton submit = (MaterialButton) findViewById(R.id.submit);
 
-        submit.setOnClickListener(new View.OnClickListener() {
+        mFullName = findViewById(R.id.fullname);
+        mEmail = findViewById(R.id.email);
+        mPhoneNumber =findViewById(R.id.phone);
+        mAddress =findViewById(R.id.address);
+        mVehicleModel = findViewById(R.id.vehiclemodel);
+        mYear = findViewById(R.id.year);
+        mVIN = findViewById(R.id.vin);
+        mPassword1 = findViewById(R.id.password1);
+        mPassword2 = findViewById(R.id.password2);
+        mSubmit = findViewById(R.id.submit);
+        mAuth = FirebaseAuth.getInstance();
+        mProgressbar = findViewById(R.id.progressbar);
+
+
+
+
+        mSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openBankInfo();
-                // to add the condition if the form is fields are correctly
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword1.getText().toString().trim();
+                String passwordcopy= mPassword2.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email address can not be blank");
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    mPassword1.setError("Password can not be blank");
+                    return;
+                }
+
+                if(password.length()<6){
+                    mPassword1.setError("Password must be at least six characters");
+                    return;
+                }
+                if(!password.equals(passwordcopy)){
+                  mPassword2.setError("Password must be the same");
+                  return;
+                }
+                mProgressbar.setVisibility(view.VISIBLE);
+
+                //registration
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Registration.this, "Account Created.",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),BankInfo.class));
+                        }else{
+                            //Toast.makeText(Registration.this, "missing fields"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
             }
         });
     }
 
-    public void openBankInfo(){
-        Intent intentreg = new Intent(this,BankInfo.class);
-        startActivity(intentreg);
-    }
 }
