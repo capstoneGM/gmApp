9781f9a73803c9d3fa1663df9ab67c2848086ab3
scanales01 <scanal01@outlook.com>
2022-03-29 18:12:15 -0500

    Update to Styles for Some Pages
    Change to Main Menu, Account info, Charging Station Info, Balance

--- GMev/app/src/main/java/com/example/gmev/ChargingStationInfo.java
+++ GMev/app/src/main/java/com/example/gmev/ChargingStationInfo.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ChargingStationInfo extends AppCompatActivity {
 
@@ -29,32 +32,32 @@ public class ChargingStationInfo extends AppCompatActivity {
         etChargerType = findViewById(R.id.chargerType);
 
         if (id.equals("1")) {
-            ((EditText)findViewById(R.id.price)).setText("$0.1331/kWH");
-            ((EditText)findViewById(R.id.chargingStationAddress)).setText("1636 S 48th St, Springdale, AR 72762");
-            ((EditText)findViewById(R.id.timeOpen)).setText("24 hours");
-            ((EditText)findViewById(R.id.numStations)).setText("Available: 0/2");
-            ((EditText)findViewById(R.id.chargerType)).setText("J1772");
+            ((TextView)findViewById(R.id.price)).setText("$0.1331/kWH");
+            ((TextView)findViewById(R.id.chargingStationAddress)).setText("1636 S 48th St, Springdale, AR 72762");
+            ((TextView)findViewById(R.id.timeOpen)).setText("24 hours");
+            ((TextView)findViewById(R.id.numStations)).setText("Available: 0/2");
+            ((TextView)findViewById(R.id.chargerType)).setText("J1772");
         }
         else if (id.equals("2")) {
-            ((EditText)findViewById(R.id.price)).setText("$0.20021/KWH");
-            ((EditText)findViewById(R.id.chargingStationAddress)).setText("4280 S Pleasant Crossing Blvd, Rogers, AR 72758");
-            ((EditText)findViewById(R.id.timeOpen)).setText("24 hours");
-            ((EditText)findViewById(R.id.numStations)).setText("Available: 8/8");
-            ((EditText)findViewById(R.id.chargerType)).setText("CCS");
+            ((TextView)findViewById(R.id.price)).setText("$0.20021/KWH");
+            ((TextView)findViewById(R.id.chargingStationAddress)).setText("4280 S Pleasant Crossing Blvd, Rogers, AR 72758");
+            ((TextView)findViewById(R.id.timeOpen)).setText("24 hours");
+            ((TextView)findViewById(R.id.numStations)).setText("Available: 8/8");
+            ((TextView)findViewById(R.id.chargerType)).setText("CCS");
         }
         else if (id.equals("3")) {
-            ((EditText)findViewById(R.id.price)).setText("$0.83032/KWH");
-            ((EditText)findViewById(R.id.chargingStationAddress)).setText("3000 Gene George Blvd, Springdale, AR 72762");
-            ((EditText)findViewById(R.id.timeOpen)).setText("8:00 AM - 10:30 PM");
-            ((EditText)findViewById(R.id.numStations)).setText("Available: 1/2");
-            ((EditText)findViewById(R.id.chargerType)).setText("TESLA");
+            ((TextView)findViewById(R.id.price)).setText("$0.83032/KWH");
+            ((TextView)findViewById(R.id.chargingStationAddress)).setText("3000 Gene George Blvd, Springdale, AR 72762");
+            ((TextView)findViewById(R.id.timeOpen)).setText("8:00 AM - 10:30 PM");
+            ((TextView)findViewById(R.id.numStations)).setText("Available: 1/2");
+            ((TextView)findViewById(R.id.chargerType)).setText("TESLA");
         }
         else {
-            ((EditText)findViewById(R.id.price)).setText("$0.0910223/KWH");
-            ((EditText)findViewById(R.id.chargingStationAddress)).setText("1636 S 48th St, Springdale, AR 72762");
-            ((EditText)findViewById(R.id.timeOpen)).setText("12:00 AM - 3:00 PM");
-            ((EditText)findViewById(R.id.numStations)).setText("Available: 4/4");
-            ((EditText)findViewById(R.id.chargerType)).setText("CHAdeMO");
+            ((TextView)findViewById(R.id.price)).setText("$0.0910223/KWH");
+            ((TextView)findViewById(R.id.chargingStationAddress)).setText("1636 S 48th St, Springdale, AR 72762");
+            ((TextView)findViewById(R.id.timeOpen)).setText("12:00 AM - 3:00 PM");
+            ((TextView)findViewById(R.id.numStations)).setText("Available: 4/4");
+            ((TextView)findViewById(R.id.chargerType)).setText("CHAdeMO");
         }
 
     }
